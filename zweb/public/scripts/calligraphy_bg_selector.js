61f5bef75d33f74f6d579a290a095a43b4f4596c
Hilal <hilal_h@mit.edu>
2024-03-29 13:24:00 -0400

    updated webside

--- zweb/public/scripts/calligraphy_bg_selector.js
+++ zweb/public/scripts/calligraphy_bg_selector.js
@@ -1,7 +1,8 @@
 window.onload = function() {
     var images = [
-        '../img/landing_bg/zweb_bg0.png',
-        '../img/landing_bg/zweb_bg1.png',
+        '../img/calligraphy_bg/c_bg0.png',
+        '../img/calligraphy_bg/c_bg1.png',
+        '../img/calligraphy_bg/c_bg2.png',
     ];
 
     var index = Math.floor(Math.random() * images.length);
